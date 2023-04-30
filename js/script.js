a09c0044db91ebef0a8a5525b57f571e96bf06f2
ChoByungHyun <102468625+ChoByungHyun@users.noreply.github.com>
2023-04-30 22:54:18 +0900

    [Feat] 팀짜기 기능 구현. 전체인원수의 과반수이상으로 팀 설정시 버그 인원 제대로 안나눠지는 버그.. 등등 수정 필요

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
 const person = document.querySelector("#person");
 const personCnt = document.querySelector("#count");
 const btnPersonName = document.querySelector(".btn-person_name");
-const btnCount = document.querySelector("#btn-count");
-const memberWrap = document.querySelector(".member-wrap");
-const memberNameWrap = document.querySelector(".member-name-wrap");
-
-const nameWrap = document.createElement("div");
+const btnCount = document.querySelector(".btn-count");
+const teamWrap = document.querySelector(".team-wrap");
+const mainWrap = document.querySelector(".wrap");
+const nameWrap = document.querySelector(".name-wrap");
 
 let allPeople = JSON.parse(localStorage.getItem("allPeople"));
 allPeople = allPeople ?? [];
 
-nameWrap.classList.add("name-wrap");
-memberWrap.appendChild(nameWrap);
+const memberWrap = document.createElement("div");
+
+nameWrap.style.fontSize = "30px";
+teamWrap.appendChild(memberWrap);
+
+memberWrap.classList.add("member-wrap");
+
+// teamNameWrap.appendChild(teamMemberList);
 
 render();
 
@@ -24,8 +29,79 @@ btnPersonName.addEventListener("click", () => {
 });
 
 function render() {
+  //   teamMemberList.textContent = "";
   nameWrap.textContent = "";
   allPeople.forEach((element) => {
-    nameWrap.textContent += element.name + ",";
+    // teamMemberList.textContent += element.name + " ";
+    nameWrap.textContent += element.name + " ";
   });
+}
+
+btnCount.addEventListener("click", () => {
+  let personCount = personCnt.value;
+  let teamCount = Math.floor(allPeople.length / personCount);
+
+  if (teamCount === 1) {
+    teamCount++;
+  }
+
+  for (let i = 0; i < teamCount; i++) {
+    const teamNameWrap = document.createElement("div");
+    teamNameWrap.classList.add("team-name-wrap");
+    memberWrap.appendChild(teamNameWrap);
+
+    const teamName = document.createElement("h2");
+    teamNameWrap.appendChild(teamName);
+    teamName.classList.add("team-name");
+    teamName.textContent = `${i + 1}팀 (${allPeople.length}명)`;
+  }
+
+  let shakeTeam = [];
+
+  for (let i = 0; i < allPeople.length; i++) {
+    shakeTeam.push(allPeople[i].name);
+  }
+  shuffle(shakeTeam);
+
+  const nameWrapDiv = document.querySelectorAll(".team-name-wrap");
+  const memberWrapDiv = document.querySelectorAll(".team-member-list");
+
+  const lastElement = shakeTeam[0];
+
+  let cnt = 0;
+
+  shakeTeam.forEach((teamEl) => {
+    cnt = 0;
+    nameWrapDiv.forEach((wrapEl) => {
+      const teamMemberList = document.createElement("p");
+      teamMemberList.classList.add("team-member-list");
+      wrapEl.appendChild(teamMemberList);
+
+      for (let i = 0; i < personCount; i++) {
+        teamMemberList.textContent += shakeTeam.pop() + " ";
+      }
+      console.log(cnt);
+      console.log(teamCount);
+      if (allPeople.length % 2 === 1 && cnt === teamCount - 1) {
+        console.log(lastElement);
+        teamMemberList.textContent += lastElement;
+      }
+      cnt++;
+    });
+  });
+});
+
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+
+function remove() {
+  const idx = allPeople.find((item) => item.len == event.srcElement.id);
+  if (idx) {
+    allPeople.splice(
+      allPeople.findIndex((item) => item.len == idx.len),
+      1
+    );
+  }
+  localStorage.setItem("allPeople", JSON.stringify(allPeople));
 }
